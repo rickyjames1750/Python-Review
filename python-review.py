1e31b00492c8624c2aba4fe299a30931655e9aa8
rickyjames1750 <rickyjames1750@gmail.com>
2021-06-02 21:00:40 -0700

    This commit will create variables, will perform casting str to int and vice versa, and var float

--- python-review.py
+++ python-review.py
@@ -3,5 +3,28 @@
 # print("Hello") This is a commemt
 
 x = 10
+y = 'coding'
 
 print(x)
+print(y)
+
+x = str(10)
+y = int("3")
+z = float(3)
+
+print(x)
+print(y)
+print(z)
+
+x = 'coding'
+y = "coding"
+
+print(x)
+print(y)
+
+
+A = "Python"
+a = "Variables"
+
+print(A)
+print(a)
