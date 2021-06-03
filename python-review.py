21196e8f9b8d49a250e26b3de91a7fb446d1c4f3
rickyjames1750 <rickyjames1750@gmail.com>
2021-06-03 00:00:37 -0700

    This commit will add python operators

--- python-review.py
+++ python-review.py
@@ -53,3 +53,41 @@ a = ("Hello World")
 
 print(a[1])
 print(len(a))
+
+# Booleans 
+
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+a = 100
+b = 50
+
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a") 
+
+print(bool("Hello"))
+print(bool(15))
+
+
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple, cherry, banana"]))
+
+
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool())
+print(bool([]))
+print(bool({}))
+
+# Operators 
+
+a = ["apple"]
+b = ["banana"]
+
+print(a + b)
