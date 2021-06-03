2d2c4023de39649a24bab9646545798c4c0c8189
rickyjames1750 <rickyjames1750@gmail.com>
2021-06-02 21:20:11 -0700

    This commit will add examples of python strings

--- python-review.py
+++ python-review.py
@@ -1,12 +1,14 @@
-#This is a comment
+#1. This is a comment
 
 # print("Hello") This is a commemt
 
+#2. Variables 
 x = 10
 y = 'coding'
 
 print(x)
 print(y)
+
 
 x = str(10)
 y = int("3")
@@ -28,3 +30,26 @@ a = "Variables"
 
 print(A)
 print(a)
+
+# Data Types 
+
+x = 1
+y = 2.8
+
+print(type(x))
+print(type(y))
+
+# Strings 
+
+a =  """Hi I am Ricky J. Sparks, A very hard-working engineer, 
+totally committed to excellence, and also someone who wants to continually improve 
+and develop my skillset to ensure I maintain competence in my role. 
+I will be able to start contributing positively towards the team's goals, 
+whilst also bringing a positive, can-do attitude to the organization and those I am working alongside."""
+
+print(a)
+
+a = ("Hello World")
+
+print(a[1])
+print(len(a))
