b5fa75586652970bf762dd91a6e5561c9824a2ad
rickyjames1750 <rickyjames1750@gmail.com>
2021-06-07 20:20:43 -0700

    This commit will create python conditional statements

--- python-review.py
+++ python-review.py
@@ -143,3 +143,16 @@ dictionary = {
 }
 
 print(dictionary)
+
+# Conditional Statements 
+
+a = 100 
+b = 100
+
+if b > a:
+    print("b is greater than a")
+elif a == b:
+    print("a and b are both equal")
+else: 
+    print("a is greater than b")
+
