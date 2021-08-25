d6329f1674e03182ebee94f7a81efc634e5e4ede
rickyjames1750 <rickyjames1750@gmail.com>
2021-08-24 23:56:22 -0700

    Update python-review.py
    This commit will be using a 2nd method to remove duplicates from list

--- python-review.py
+++ python-review.py
@@ -216,3 +216,27 @@ for i in test_list:
 
 # printing list after the removal
 print ("The list after removing duplicates : " + str(res))
+
+
+
+
+
+# Method 2: Using list comprehension
+
+# Code to demonstrate
+# removing duplicated from list
+# using list comprehension
+
+# initializing list
+test_list = [1, 3, 5, 6, 3, 5, 6, 1]
+print ("The original list is : " + str(test_list))
+
+# using list comprehension
+# to remove duplicated
+# from list
+res = []
+[res.append(x) for x in test_list if x not in res]
+
+# printing list after removal
+print ("The list after removing duplicates : " + str(res))
+
