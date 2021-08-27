0ded7170d7c33a908f2bdefc433a85db719c09c0
rickyjames1750 <rickyjames1750@gmail.com>
2021-08-26 23:03:22 -0700

    This commit will add method 3: set()
    Python daily warm up.

--- python-review.py
+++ python-review.py
@@ -240,3 +240,24 @@ res = []
 # printing list after removal
 print ("The list after removing duplicates : " + str(res))
 
+
+
+# Method 3 
+# Python 3 code to demonstrate
+# removing duplicated from list
+# using set()
+
+# initializing list
+test_list = [1, 5, 3, 6, 3, 5, 6, 1]
+print ("The original list is : " + str(test_list))
+
+# using set()
+# to remove duplicated
+# from list
+test_list = list(set(test_list))
+
+# printing list after removal
+# distorted ordering
+print ("The list after removing duplicates : " + str(test_list))
+
+
