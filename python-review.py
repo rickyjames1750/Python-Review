9d89af8d8283cdeeaee3d04ddd51b9a57222da62
rickyjames1750 <rickyjames1750@gmail.com>
2021-08-27 23:49:58 -0700

    This commit will create a 4th method to remove duplicates from list
    Method 4 - Using list comprehension + enumerate()

--- python-review.py
+++ python-review.py
@@ -260,4 +260,19 @@ test_list = list(set(test_list))
 # distorted ordering
 print ("The list after removing duplicates : " + str(test_list))
 
+#Method 4 - Using list comprehension + enumerate()
+# Python 3 code to demonstratef
+# removing duplicated from list
+# using list comprehension + enumerate()
 
+# initializing list
+test_list = [1, 5, 3, 6, 3, 5, 6, 1]
+print ("The original list is : " + str(test_list))
+
+# using list comprehension + enumerate()
+# to remove duplicated
+# from list
+res = [i for n, i in enumerate(test_list) if i not in test_list[:n]]
+
+# printing list after removal
+print ("The list after removing duplicates : " + str(res))
